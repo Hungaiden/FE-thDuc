6dda46753a28f004f9d39c68862ced1f9fa69c3c
Bui Duc Hung <duchungbui1@gmail.com>
2025-05-18 14:45:57 +0700

    init

---
+++ src/modelData/model.js
@@ -0,0 +1,60 @@
+export const userListModel = async () => {
+  try {
+    const response = await fetch("https://y2y8tg-8080.csb.app/users/list");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    // console.log(response.json());
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+    return 0;
+  }
+};
+
+export const userModel = async (userId) => {
+  try {
+    const response = await fetch(`https://y2y8tg-8080.csb.app/users/${userId}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return 0;
+  }
+};
+
+export const photoOfUserModel = async (userId) => {
+  try {
+    const response = await fetch(
+      `https://y2y8tg-8080.csb.app/photos/${userId}`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return 0;
+  }
+};
+
+export const schemaModel = async () => {
+  try {
+    const response = await fetch("https://y2y8tg-8080.csb.app/test/info");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return 0;
+  }
+};
